21ca88200184ce878457e1d2778de0757e9a5499
fs-rd-masakazu-mori <masakazu.mori@f-scratch.com>
2017-12-01 14:51:11 +0900

    不要コード削除

--- fukuokaai/#3/code/predict_sin.py
+++ fukuokaai/#3/code/predict_sin.py
@@ -3,7 +3,6 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers.recurrent import LSTM
 from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt
 
 def sin( x, T = 100 ):
